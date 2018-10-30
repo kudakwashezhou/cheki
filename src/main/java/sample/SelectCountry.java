b2b66fbffc12c90a1360b814e5aab21ef678f3e3
AttainB <attain@crystalqa.com>
2018-10-30 17:18:00 +0200

    initial commit SelectCountry only commit

--- src/main/java/sample/SelectCountry.java
+++ src/main/java/sample/SelectCountry.java
@@ -2,25 +2,39 @@ package sample;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 public class SelectCountry {
-    public static WebDriver driver = null;
+   // public static WebDriver driver = null;
+    @Test
+   public void invoke1(){
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("start-maximized"); // open Browser in maximized mode
+       options.addArguments("disable-infobars"); // disabling infobars
+       options.addArguments("--disable-extensions"); // disabling extensions
+       options.addArguments("--disable-gpu"); // applicable to windows os only
+       options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+       options.addArguments("--no-sandbox"); // Bypass OS security model
+       WebDriver driver = new ChromeDriver(options);
+        driver.get("https://google.com");
+   }
 
 
-    public void InvokeDriver() {
+  /*  public void InvokeDriver() {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(40L, TimeUnit.SECONDS);
+
+
+
     }
     @Test
     public void cheki(){
-        this.InvokeDriver();
-        driver.get("https://www.cheki.com/");
+        this.invoke1();
+
     }
    /* @Test
     public void testCountries() {
